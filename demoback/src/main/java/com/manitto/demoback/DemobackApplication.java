adbca1545a3adbb1adeeedabb9e53494c9abe1c6
coingye <coingye96@gmail.com>
2024-03-02 18:25:09 +0900

    React, Sprint Boot 환경 설정

---
+++ demoback/src/main/java/com/manitto/demoback/DemobackApplication.java
@@ -0,0 +1,13 @@
+package com.manitto.demoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobackApplication.class, args);
+	}
+
+}
---
+++ demoback/src/test/java/com/manitto/demoback/DemobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manitto.demoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
